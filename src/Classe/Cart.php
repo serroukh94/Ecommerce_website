59064a9acf206f15278bfe5ef67aca38075f78b2
serroukh <serroukh94@gmail.com>
2022-08-20 03:07:48 +0200

    add, decrease and remove a product in cart

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -4,15 +4,19 @@ namespace App\Classe;
 
 
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class Cart
 {
     private $session;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(RequestStack $stack)
+    public function __construct(EntityManagerInterface $entityManager, RequestStack $stack)
     {
         $this->session = $stack->getSession();
+        $this->entityManager = $entityManager;
     }
 
 
@@ -37,5 +41,50 @@ class Cart
     public function remove()
     {
         return $this->session->remove('cart');
+    }
+
+    public function delete($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart', $cart);
+    }
+
+    public function decrease($id)
+    {
+        $cart=$this->session->get('cart', []);
+
+        // vérifier si la quantité de notre produit = 1
+        if ($cart[$id] > 1){
+              $cart[$id]--;        // retirer une quantité (-1)
+        }else{
+              unset($cart[$id]);   //supprimer mon produit
+        }
+        return $this->session->set('cart', $cart);
+    }
+
+    public function getFull()   //recypérer le panier dans sa globalité
+    {
+        $cartComplete = [];
+
+        if($this->get()){
+            foreach ($this->get() as $id => $quantity) {
+                $product_object = $this->entityManager->getRepository(Product::class)->findOneById($id);
+
+                    if(!$product_object){
+                        $this->delete($id);
+                        continue;
+                    }
+
+                    $cartComplete[] = [
+                    'product' => $product_object,
+                    'quantity' => $quantity
+                ];
+            }
+        }
+
+        return $cartComplete;
     }
 }
